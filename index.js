8ea9d35b765edd2aac24ab2e7c0045cb447dd511
afifeka <37196374+afifeka@users.noreply.github.com>
2018-03-30 21:45:29 +0800

    Update index.js

--- index.js
+++ index.js
@@ -85,6 +85,30 @@ bot.on("message", async message => {
     return;
   }
   
+  if(cmd === `${prefix}addrole`){
+    if (!message.member.hasPermission("MANAGE_ROLES")) return errors.noPerms(message, "MANAGE_ROLES");
+    if (args[0] == "help") {
+      message.reply("Usage: !addrole <user> <role>");
+      return;
+    }
+    let rMember = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
+    if (!rMember) return errors.cantfindUser(message.channel);
+    let role = args.join(" ").slice(22);
+    if (!role) return message.reply("Specify a role!");
+    let gRole = message.guild.roles.find(`name`, role);
+    if (!gRole) return message.reply("Couldn't find that role.");
+  
+    if (rMember.roles.has(gRole.id)) return message.reply("They already have that role.");
+    await (rMember.addRole(gRole.id));
+  
+    try {
+      await rMember.send(`Congrats, you have been given the role ${gRole.name}`)
+    } catch (e) {
+      console.log(e.stack);
+      message.channel.send(`Congrats to <@${rMember.id}>, they have been given the role ${gRole.name}. We tried to DM them, but their DMs are locked.`)
+    }
+  }
+  
   if(cmd === `${prefix}kecoa`){
     if(!args[2]) return message.reply("**Usage `!kecoa apakah <Question>`**");
     let replies = ["Iya", "Tidak", "Saya Tidak Tahu", "Apa Yang Kamu Bilang?", "Sangat Benar", "Sangat Salah"];
