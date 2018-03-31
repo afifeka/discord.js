9b3649860b157b7a0a1f20ae6271ffa215ca3570
afifeka <37196374+afifeka@users.noreply.github.com>
2018-03-31 12:12:35 +0800

    Update index.js

--- index.js
+++ index.js
@@ -153,6 +153,21 @@ bot.on("message", async message => {
     message.channel.send(ballembed)
 
   }
+  
+  if(cmd === `${prefix}userinfo`){
+    const member = message.mentions.members.first() || message.guild.members.get(args[0]) || message.member;
+    let embed = new Discord.RichEmbed()
+    .setDescription("**USER INFO**")
+    .setColor("#00a6ff")
+    .setImage(member.user.displayAvatarURL)
+    .addField(":bust_in_silhouette: | Player", `${member.user.tag}`)
+    .addField(":shield: | ID", member.id)
+    .addField(":hammer: | Created", member.user.createdAt)
+    .addField(":inbox_tray: | Joined", member.joinedAt);
+
+    message.channel.send(embed);
+    return;
+  }
 
 
   if(cmd === `${prefix}report`){
@@ -251,8 +266,8 @@ bot.on("message", async message => {
     let helpembed = new Discord.RichEmbed()
     .setColor("#15f153")
     .setDescription("**Prefix : `!`**")
-    .addField(":lock: Moderators Command!", "| `!ban` | `!kick` | `!tempmute` | `!say` | `!clear` | `!news` | `!warn` | `!addrole help` | `!removerole help` | `!userinfo` |")
-    .addField(":earth_asia: General Command", "| `!botinfo` | `!serverinfo` | `!ping` | `!afk` | `!help` | `!kecoa apakah (question)` |")
+    .addField(":lock: Moderators Command!", "| `!ban` | `!kick` | `!tempmute` | `!say` | `!clear` | `!news` | `!warn` | `!addrole help` | `!removerole help` |")
+    .addField(":earth_asia: General Command", "| `!botinfo` | `!serverinfo` | `!ping` | `!afk` | `!help` | `!kecoa apakah (question)` | `!userinfo` |")
     .addField(":musical_note: Music Command", "| `Command Not Found!` |")
     .setFooter("Beta v0.2 | Discord.js");
     message.delete().catch(O_o=>{});
