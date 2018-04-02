09f345f42d3d1381a9ab1fa58075fd88b2bbdc06
afifeka <37196374+afifeka@users.noreply.github.com>
2018-04-02 19:47:56 +0700

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cpu = process.cpuUsage().system / 1024 / 1024;
 const used = process.memoryUsage().heapUsed / 1024 / 1024;
 const ms = require("ms");
 const YTDL = require("ytdl-core");
+const weather = require("weather-js");
 
 const bot = new Discord.Client({disableEveryone: true});
 
@@ -510,6 +511,34 @@ bot.on("message", async message => {
 
     if (message.guild.voiceConnection) message.guild.voiceConnection.disconnect();
   }
+  
+  if(cmd === `${prefix}weather`){
+    const city = message.content.split(" ").slice(1).join(" ")
+    if (!city) return message.channel.send("**Error**\nYou did not include a city! Please include it so we can show the forecast!")
+
+    weather.find({search: city, degreeType: 'F'}, function(err, result) {
+        if (err) {
+            message.channel.send(":x: No results on that city :x:")
+            console.log(err.stack)
+            return;
+        } 
+        let url;
+        if (result[0].current.skytext === "Mostly Sunny") url = "https://openclipart.org/image/2400px/svg_to_png/3367/ivak-Decorative-Sun.png"
+        else if (result[0].current.skytext === "Mostly Cloudy" || result[0].current.skytext === "Cloudy") url = "https://upload.wikimedia.org/wikipedia/commons/thumb/3/35/Weather-heavy-overcast.svg/200px-Weather-heavy-overcast.svg.png"
+        else if (result[0].current.skytext === "Partly Cloudy") url = "";
+        message.channel.send(JSON.stringify(result[0].current, null, 2))
+        var embed = new Discord.RichEmbed()
+        .setTitle(`Forecast for ${result[0].location.name}`)
+        .setColor("BLUE")
+        .setThumbnail(result[0].current.imageUrl)
+        .setTimestamp()
+        .addField(":thermometer: Temperature :thermometer:", `**__${result[0].current.temperature}__ Degrees Fahrenheit**`)
+        .addField(":city_sunset: What it looks like outside :city_sunset:", `**__${result[0].current.skytext}__**`)
+        .addField(":wind_blowing_face: Feels Like :wind_blowing_face:", `**__${result[0].current.feelslike}__ Degrees Fahrenhiet**`)
+        .addField(":sweat: Humidity :sweat:", `**__${result[0].current.humidity}%__**`)
+        .addField(":wind_blowing_face: Wind Speed :wind_blowing_face:", `**__${result[0].current.windspeed.replace("mph", "Miles Per Hour")}__**`)
+        message.channel.send({ embed: embed })
+   })};
 
 });
   
