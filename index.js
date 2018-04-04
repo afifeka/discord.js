6cc0ce0fdfea5319e06492625372ea246b9985e4
afifeka <37196374+afifeka@users.noreply.github.com>
2018-04-04 20:53:31 +0700

    Update index.js

--- index.js
+++ index.js
@@ -563,33 +563,6 @@ bot.on("message", async message => {
    message.channel.send(`Created role ${name}!`)
    }
   
-  if(cmd === `${prefix}purge`){
-        if(!message.member.hasPermission('MANAGE_MESSAGES')) {
-        return message.channel.send("You need the permission **MANAGE_MESSAGES** to use this command!")
-    }
-    if (isNaN(args[0])) return message.channel.send({
-            embed: {
-                title: "ERROR!",
-                color: 0xE50000,
-                description: "Missing `<number>` argument."
-            }
-        })
-        
-    if (args[0] > 100) return message.channel.send({
-            embed: {
-                title: "ERROR!",
-                color: 0xE50000,
-                description: "Please supply a number less than 100."        
-    message.channel.bulkDelete(args[0])
-        .then( messages => message.channel.send({
-            embed: {
-                title: "SUCCESS!",
-                color: 0x20db27,
-                description: `I have deleted **${args[0]}** messages.`
-            }
-        }).then( msg => msg.delete({ timeout: 10000 })));
-    
-}
 
 
 
