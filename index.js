a45375c36d570119d926752c5e78f417566759c6
afifeka <37196374+afifeka@users.noreply.github.com>
2018-04-04 19:46:42 +0700

    Update index.js

--- index.js
+++ index.js
@@ -171,7 +171,7 @@ bot.on("message", async message => {
 
   if(cmd === `${prefix}addrole`){
     if (!message.member.hasPermission("MANAGE_ROLES")) return errors.noPerms(message, "MANAGE_ROLES");
-    if (args[0] == "help") {
+    if (args[1] == "help") {
       message.reply("Usage: !addrole <user> <role>");
       return;
     }
@@ -321,7 +321,7 @@ bot.on("message", async message => {
 
   if(cmd === `${prefix}say`){
     if(!message.member.hasPermission("ADMINISTRATOR")) return;
-    const sayMessage = args[1];
+    const sayMessage = args[0];
     message.delete().catch();
     message.channel.send(sayMessage);
   }
@@ -579,10 +579,7 @@ bot.on("message", async message => {
             embed: {
                 title: "ERROR!",
                 color: 0xE50000,
-                description: "Please supply a number less than 100."
-            }
-        });
-        
+                description: "Please supply a number less than 100."        
     message.channel.bulkDelete(args[0])
         .then( messages => message.channel.send({
             embed: {
@@ -593,48 +590,7 @@ bot.on("message", async message => {
         }).then( msg => msg.delete({ timeout: 10000 })));
     
 }
-  if(cmd === `${prefix}addrole2`){
-    	const args = message.content.split(' ').slice(2).join(' ');
-			var embed = new Discord.RichEmbed()
-			.setTitle("Denied Acess :x:")
-			.setColor(0xff3200)
-			.setThumbnail("http://i0.kym-cdn.com/photos/images/newsfeed/001/234/765/971.gif")
-			.addField("You are not permitted to use this command...", "If you do have the permission `MANAGE_ROLES_OR_PERMISSIONS` please contact the bot creators")
-			    if (!message.member.hasPermission("MANAGE_ROLES_OR_PERMISSIONS")) return message.channel.send({ emebd: embed });
-let member = message.mentions.members.first();
-var embed2 = new Discord.RichEmbed()
-.setTitle("No @Mention :x:")
-.setColor(0xff3200)
-.setThumbnail("http://i0.kym-cdn.com/photos/images/newsfeed/001/234/765/971.gif")
-.addField("You did not mention anyone", "You need to mention someone for this command to work, you can do this by searching someone while typing @")
-if (!member) return message.channel.send({ emebd: embed2 });
-var embed3 = new Discord.RichEmbed()
-.setTitle("No Role Specified :x:")
-.setColor(0xff3200)
-.setThumbnail("http://i0.kym-cdn.com/photos/images/newsfeed/001/234/765/971.gif")
-.addField("You did not specify a role", "Or arguments apparently...") 
-if (!args) return message.channel.send({ emebd: embed3 });
-let myRole = message.guild.roles.find("name", `${args}`);
-var embed4 = new Discord.RichEmbed()
-.setTitle("Role does not exist :x:")
-.setThumbnail("http://i0.kym-cdn.com/photos/images/newsfeed/001/234/765/971.gif")
-.setColor(0xff3200)
-.addField("That role does not exist!", "If it does, it ***__MUST__*** be case sensitive!")
-if (!myRole) return message.channel.send({ emebd: embed4 });
-var embed5 = new Discord.RichEmbed()
-.setTitle("Insufficent Permissions for me")
-.setColor(0xff3200)
-.setThumbnail("https://www.viralviralvideos.com/wp-content/uploads/2017/06/Facepalm-GIF-2015.gif")
-.addField("I do not have the correct permissions to add a role!", "Please change it... Please.... :pray: ")
-     if (!message.guild.member(bot.user).hasPermission('MANAGE_ROLES_OR_PERMISSIONS')) return message.channel.send({ emebd: embed5 });
-     var embed6 = new Discord.RichEmbed()
-.setTitle("Added the role!")
-.setTimestamp()
-.setImage("https://media.giphy.com/media/l41lNm5py4150DOwg/giphy.gif")
-.setColor(0x7bff00)
-.addField(`Sucessfuly added ${member.user.username} the role ${args}!`, "Have fun!")
-member.addRole(myRole).then(message.channel.send({ embed: embed6 }));
-}
+
 
 
 });
