2ac7dfaca7ac2ffa121ba249b906d70ed8684a3c
afifeka <37196374+afifeka@users.noreply.github.com>
2018-04-06 05:42:08 +0700

    Update index.js

--- index.js
+++ index.js
@@ -671,57 +671,51 @@ bot.on("message", async message => {
     }) 
     
   }
+  
+  if(cmd === `${prefix}tableflip`){
+	message.channel.send("(°-°)\\ ┬─┬").then(m => {
+        setTimeout(() => {
+            m.edit("(╯°□°)╯    ]").then(ms => {
+                setTimeout(() => {
+                    ms.edit("(╯°□°)╯  ︵  ┻━┻")
+                }, 250)
+            })
+        }, 250);
 
-  if(cmd === `${prefix}clear`){
-	    if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("No.");
-  if(!args[0]) return message.channel.send("no");
-  message.channel.bulkDelete(args[0]).then(() => {
-  message.channel.send(`Clear ${args[0]} messages.`).then(msg => msg.delete(2000));
-});
+    });
+    
 }
-  if(cmd === `${prefix}reload){
- var embedNoWork = new Discord.RichEmbed()
-  .setTitle("Restricted")
-    .setColor("#f45f42")
-  .addField("You are restricted from this command", "Its for the bot owners only!")
-    
-    var authors = ["396027480097554432", "262410813254402048"];
-    if(!authors.includes(message.author.id)) {
-    message.channel.send({embed: embedNoWork});
-    }
-    
-  const term = require( 'terminal-kit' ).terminal ;
-
-  if (!args || args.length < 1) return message.channel.send("Must provide a command name to reload!");
-
-    delete require.cache[require.resolve(`./${args[0]}.js`)];
-    message.channel.send("Succesfully reloaded " + `${args[0]}`)
-
-let progressBar , progress = 0 ;
-
-  function doProgress()
-  {
-    progress += Math.random() / 10 ;
-    progressBar.update( progress ) ; 
-
-    if ( progress >= 1 )
-    {
-    console.log(`The command ${args[0]} has been reloaded`)
-    }
-    else
-    {
-      setTimeout( doProgress , 100 + Math.random() * 400 ) ;
-    }
+   if(cmd === `${prefix}achieve`){
+	             var ids = [
+            "20",
+            "1",
+            "13",
+            "18",
+            "17",
+            "9",
+            "31",
+            "22",
+            "23",
+            "2",
+            "11",
+            "19",
+            "24",
+            "25",
+            "12",
+            "33"
+            ]
+            const randomizer = Math.floor(Math.random()*ids.length);
+            const args = message.content.split(" ").slice(1).join(" ")
+    if (!args) return message.channel.send("Put something you want to achieve!");
+    const image = new Discord.Attachment(`https://www.minecraftskinstealer.com/achievement/a.php?i=${ids[randomizer]}&h=Achievement Get!&t=${args}`, "achievement.png");
+    message.channel.send(image)
   }
-
-
-  progressBar = term.progressBar({
-    width: 80 ,
-    title: 'Reloading Command '+args[0]+':' ,
-    eta: true ,
-    percent: true
-  });
-  doProgress();
+  if(cmd === `${prefix}lmgtfy`){
+	      let question = encode(args.join(' '));
+    
+    let link = `https://www.lmgtfy.com/?q=${question}`;
+    
+    message.channel.send(`**<${link}>**`)
 }
 
 	
